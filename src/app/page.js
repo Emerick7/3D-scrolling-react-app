22682fd4857fb9d906c6e5a9dbbd598cd4c85bf4
Emerick Fouchard <fouchardemerick@gmail.com>
2024-05-21 15:10:51 +0530

    3D models and animations on scroll done

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
-import Image from "next/image";
+'use client'
 import styles from "./page.module.css";
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/experience";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <Canvas className={styles.canvas} camera={{
+        fov: 64,
+        position: [2.3, 1.5, 2.3]}}>
+        <Experience/>
+      </Canvas>
     </main>
   );
 }
